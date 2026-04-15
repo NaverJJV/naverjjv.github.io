3421e4f782ae91369bc560cd197cc81fce162217
NaverJJV <realjjv@outlook.com>
2026-04-14 23:40:15 -0500

    Add more photos to carousel

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,6 +9,10 @@ const Hero = () => {
         "/photo1.jpg",
         "/photo2.jpg",
         "/photo3.jpg",
+        "/photo4.jpg",
+        "/photo5.jpg",
+        "/photo6.jpg",
+        "/photo7.jpg",
     ];
 
     const techStack = [
